f7b7a3fa2b3e2ae2560ed691fbebba626adc8f13
Wojciech Migda <wojtek.golf@interia.pl>
2015-02-09 22:41:01 +0100

    feature remapping and code decluttering

--- src/TripSafetyFactors.hpp
+++ src/TripSafetyFactors.hpp
@@ -34,8 +34,35 @@
 #include <iostream>
 #include <cstdlib>
 #include <iterator>
+#include <map>
 
 typedef double real_type;
+
+std::map<real_type, real_type>
+map_event_density(
+    const std::valarray<real_type> & feat,
+    const std::valarray<real_type> & y
+)
+{
+    std::map<real_type, std::pair<num::size_type, num::size_type>> event_count;
+
+    assert(feat.size() == y.size());
+
+    for (num::size_type r{0}; r < feat.size(); ++r)
+    {
+        event_count[feat[r]].first++;
+        event_count[feat[r]].second += y[r];
+    }
+
+    std::map<real_type, real_type> result;
+
+    for (auto count : event_count)
+    {
+        result[count.first] = (real_type)count.second.second / count.second.first;
+    }
+
+    return result;
+}
 
 std::vector<int> do_log_reg(
     num::array2d<real_type> && i_X_train,
@@ -43,44 +70,124 @@ std::vector<int> do_log_reg(
     num::array2d<real_type> && i_X_test
 )
 {
-    std::vector<int> result(i_X_test.shape().first);
+    typedef num::array2d<real_type> array_type;
+    typedef std::valarray<real_type> vector_type;
 
     const num::size_type NUM_FEAT{i_X_train.shape().second + 1};
 
-    // let's map input features onto what we'll work with
-    num::array2d<real_type> X_train =
-        num::ones<real_type>(num::shape_type{i_X_train.shape().first, NUM_FEAT});
+    // let's map input training features onto what we'll work with
+    // first column will be 1s for the intercept
+    array_type X_train = num::ones<real_type>({i_X_train.shape().first, NUM_FEAT});
 
-    X_train[X_train.columns(1, X_train.shape().second - 1)] =
-        i_X_train[i_X_train.columns(0, i_X_train.shape().second - 1)];
+    // the rest will be copied from i_X_train
+    // X_train[:, 1:] = i_X_train[:, :]
+    X_train[X_train.columns(1, -1)] = i_X_train[i_X_train.columns(0, -1)];
 
     // same with test features
-    num::array2d<real_type> X_test =
-        num::ones<real_type>(num::shape_type{i_X_test.shape().first, NUM_FEAT});
+    array_type X_test = num::ones<real_type>({i_X_test.shape().first, NUM_FEAT});
 
-    X_test[X_test.columns(1, X_test.shape().second - 1)] =
-        i_X_test[i_X_test.columns(0, i_X_test.shape().second - 1)];
+    // X_test[:, 1:] = i_X_test[:, :]
+    X_test[X_test.columns(1, -1)] = i_X_test[i_X_test.columns(0, -1)];
 
-    std::valarray<real_type> y_train = i_y_train.apply([](real_type v){return v > 1.0 ? 1.0 : v;});
+    // we got train and test features mapped
 
-    std::valarray<real_type> theta(0.0, X_train.shape().second);
-
+    // lets keep columnd indices under control
+    enum col : num::size_type
     {
-        // testing
-        for (num::size_type c{1}; c < X_train.shape().second; ++c)
+        INTERCEPT,
+        SOURCE, // *
+        DIST,
+        CYCLES,
+        COMPLEXITY,
+        CARGO, // *
+        STOPS,
+        START_DAY,
+        START_MONTH,
+        START_DAY_OF_MONTH,
+        START_DAY_OF_WEEK,
+        START_TIME,
+        DAYS,
+        PILOT, // *
+        PILOT2, // *
+        PILOT_EXP,
+        PILOT_VISITS_PREV,
+        PILOT_HOURS_PREV,
+        PILOT_DUTY_HOURS_PREV,
+        PILOT_DIST_PREV,
+        ROUTE_RISK_1,
+        ROUTE_RISK_2,
+        WEATHER, // *
+        VISIBILITY,
+        TRAF0,
+        TRAF1,
+        TRAF2,
+        TRAF3,
+        TRAF4,
+    };
+
+    // There are some features (marked with asterix above) which do not have
+    // a meaning in a monotonic domain. Let's do the following experiment:
+    // for each of those features map to each occuring value a pair of values -
+    // a number of occurences and a sum of events,
+    // then we remap the original values to event density
+
+    for (auto COLUMN :
+        std::initializer_list<int>
         {
-            const std::valarray<real_type> & col = X_train[X_train.column(c)];
-            const std::valarray<real_type> & colt = X_test[X_test.column(c)];
+            col::SOURCE,
+            col::CARGO,
+            col::PILOT,
+            col::PILOT2,
 
-            const real_type mu = num::mean<real_type>(col);
-            const real_type dev = num::std<real_type>(col);
-
-            X_train[X_train.column(c)] = col - mu;
-            X_train[X_train.column(c)] = col / dev;
-
-            X_test[X_test.column(c)] = colt - mu;
-            X_test[X_test.column(c)] = colt / dev;
+//            col::START_DAY, // ---
+            col::START_MONTH, // +++
+//            col::START_DAY_OF_MONTH, // ---
+//            col::START_DAY_OF_WEEK, // ---
+//            col::WEATHER
         }
+    )
+    {
+        auto event_density = map_event_density(X_train[X_train.column(COLUMN)], i_y_train);
+        std::cerr << "event density size: " << event_density.size() << std::endl;
+
+        vector_type mapped_train_col = X_train[X_train.column(COLUMN)];
+        std::transform(std::begin(mapped_train_col), std::end(mapped_train_col), std::begin(mapped_train_col),
+            [&event_density](const real_type & x) -> real_type
+            {
+                return event_density[x];
+            }
+        );
+        X_train[X_train.column(COLUMN)] = mapped_train_col;
+
+        vector_type mapped_test_col = X_test[X_test.column(COLUMN)];
+        std::transform(std::begin(mapped_test_col), std::end(mapped_test_col), std::begin(mapped_test_col),
+            [&event_density](const real_type & x) -> real_type
+            {
+                return event_density[x];
+            }
+        );
+        X_test[X_test.column(COLUMN)] = mapped_test_col;
+    }
+
+
+    vector_type y_train = i_y_train.apply([](real_type v){return v > 1.0 ? 1.0 : v;});
+
+    vector_type theta(0.0, X_train.shape().second);
+
+    // standardization
+    for (num::size_type c{1}; c < X_train.shape().second; ++c)
+    {
+        const vector_type & col = X_train[X_train.column(c)];
+        const vector_type & colt = X_test[X_test.column(c)];
+
+        const real_type mu = num::mean<real_type>(col);
+        const real_type dev = num::std<real_type>(col);
+
+        X_train[X_train.column(c)] = col - mu;
+        X_train[X_train.column(c)] = col / dev;
+
+        X_test[X_test.column(c)] = colt - mu;
+        X_test[X_test.column(c)] = colt / dev;
     }
 
     num::LogisticRegression<real_type> logRegClassifier(
@@ -98,27 +205,28 @@ std::vector<int> do_log_reg(
     auto pred = logRegClassifier.predict(X_test, fit_theta, false);
 //    std::copy(std::begin(pred), std::end(pred), std::ostream_iterator<real_type>(std::cerr, "\n"));
     std::cerr << "pred.max() " << pred.max() << std::endl;
-    {
-        std::vector<std::pair<num::size_type, real_type>> zipped;
-        zipped.reserve(pred.size());
 
-        for (num::size_type idx = 0; idx < pred.size(); ++idx)
-        {
-            zipped.push_back(std::pair<num::size_type, real_type>(idx + 1, pred[idx]));
-        }
-        std::sort(zipped.begin(), zipped.end(),
-            [](const std::pair<num::size_type, real_type> & p, const std::pair<num::size_type, real_type> & q)
-            {
-                return p.second > q.second;
-            }
-        );
-        std::transform(zipped.cbegin(), zipped.cend(), result.begin(),
-            [](const std::pair<num::size_type, real_type> & p)
-            {
-                return (int)p.first;
-            }
-        );
+    std::vector<int> result(i_X_test.shape().first);
+
+    std::vector<std::pair<num::size_type, real_type>> zipped;
+    zipped.reserve(pred.size());
+
+    for (num::size_type idx = 0; idx < pred.size(); ++idx)
+    {
+        zipped.push_back(std::pair<num::size_type, real_type>(idx + 1, pred[idx]));
     }
+    std::sort(zipped.begin(), zipped.end(),
+        [](const std::pair<num::size_type, real_type> & p, const std::pair<num::size_type, real_type> & q)
+        {
+        return p.second > q.second;
+        }
+    );
+    std::transform(zipped.cbegin(), zipped.cend(), result.begin(),
+        [](const std::pair<num::size_type, real_type> & p)
+        {
+        return (int)p.first;
+        }
+    );
 
     return result;
 }
@@ -173,6 +281,10 @@ TripSafetyFactors::predict(
         STABILITY_CNT,
         EVT_CNT
     };
+
+    typedef num::array2d<real_type> array_type;
+    typedef std::valarray<real_type> vector_type;
+
     const num::size_type TRAIN_ROWS{i_train_data.size()};
     constexpr num::size_type TRAIN_COLS{34};
 
@@ -182,12 +294,7 @@ TripSafetyFactors::predict(
     const num::shape_type TRAIN_SHAPE{TRAIN_ROWS, TRAIN_COLS};
     const num::shape_type TEST_SHAPE{TEST_ROWS, TEST_COLS};
 
-    std::vector<int> result(TEST_ROWS);
-
-    //////////////
-    std::iota(result.begin(), result.end(), 1);
-    std::random_shuffle(result.begin(), result.end());
-    //////////////
+    ////////////////////////////////////////////////////////////////////////////
 
     auto time_xlt = [](const char * str) -> real_type
     {
@@ -199,42 +306,45 @@ TripSafetyFactors::predict(
         }
         return result;
     };
-    num::array2d<real_type> train_data =
+    array_type train_data =
         num::loadtxt(
             std::move(i_train_data),
             std::move(
                 num::loadtxtCfg<real_type>()
                 .delimiter(',')
-                .converters(num::loadtxtCfg<real_type>::converters_type{{col::START_TIME, time_xlt}})
+                .converters({{col::START_TIME, time_xlt}})
             )
         );
     std::cerr << train_data.shape() << std::endl;
 
-    num::array2d<real_type> test_data =
+    array_type test_data =
         num::loadtxt(
             std::move(i_test_data),
             std::move(
                 num::loadtxtCfg<real_type>()
                 .delimiter(',')
-                .converters(num::loadtxtCfg<real_type>::converters_type{{col::START_TIME, time_xlt}})
+                .converters({{col::START_TIME, time_xlt}})
             )
         );
     std::cerr << test_data.shape() << std::endl;
 
-    num::array2d<real_type> X_train_data(num::shape_type{train_data.shape().first, col::TRAF4 - col::SOURCE + 1}, 0.0);
-    X_train_data[X_train_data.columns(0, X_train_data.shape().second - 1)] =
-        train_data[train_data.columns(col::SOURCE, col::TRAF4)];
+    ////////////////////////////////////////////////////////////////////////////
 
-    std::valarray<real_type> y_train_data = train_data[train_data.column(col::EVT_CNT)];
+    array_type X_train_data({train_data.shape().first, col::TRAF4 - col::SOURCE + 1}, 0.0);
 
-    num::array2d<real_type> X_test_data(num::shape_type{test_data.shape().first, test_data.shape().second - 1}, 0.0);
-    X_test_data[X_test_data.columns(0, X_test_data.shape().second - 1)] =
-        test_data[test_data.columns(col::SOURCE, col::TRAF4)];
+    X_train_data[X_train_data.columns(0, -1)] = train_data[train_data.columns(col::SOURCE, col::TRAF4)];
+
+    vector_type y_train_data = train_data[train_data.column(col::EVT_CNT)];
+
+    array_type X_test_data({test_data.shape().first, test_data.shape().second - 1}, 0.0);
+
+    X_test_data[X_test_data.columns(0, -1)] = test_data[test_data.columns(col::SOURCE, col::TRAF4)];
+
+    ////////////////////////////////////////////////////////////////////////////
+
+    std::vector<int> result(TEST_ROWS);
 
     result = do_log_reg(std::move(X_train_data), std::move(y_train_data), std::move(X_test_data));
-
-//    void foo();
-//    foo();
 
     return result;
 }
